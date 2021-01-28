d309e56485b8969e87cc79100781a80ea3d72a6d
vinayaachar <vinayaachar@gmail.com>
2021-01-27 20:54:33 -0800

    Basic Setup of Server and Client Complete

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+app.use(express.static('./client'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/', (req,res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+})
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
