f7890dab986982f1f9289a1747b89a751dc5b73c
vinayaachar <vinayaachar@gmail.com>
2021-01-30 12:41:20 -0800

    Complete Bare Minimum Requirements

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
+$(.button).on('click', function () {
+  const serverUrl = 'http://127.0.0.1:8080/'
+  $.ajax({
+    url: server.js,
+    type: 'POST',
 
+    data: new FormData($('form')[0]),
+    cache: false,
+    contentType: false,
+    processData: false,
+
+    xhr: function () {
+      var myXhr = $.ajaxSettings.xhr();
+      if (myXhr.upload) {
+        // For handling the progress of the upload
+        myXhr.upload.addEventListener('progress', function (e) {
+          if (e.lengthComputable) {
+            $('progress').attr({
+              value: e.loaded,
+              max: e.total,
+            });
+          }
+        }, false);
+      }
+      return myXhr;
+    }
+
+  })
+})
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -5,36 +5,58 @@ const bodyParser = require('body-parser');
 const { pathToFileURL } = require('url');
 app.use(express.static('./client'));
 app.use(bodyParser.urlencoded({ extended: true }));
-var path = require('path');
+const path = require('path');
+const multer = require('multer');
+const formidable = require('formidable');
+app.use(express.static(__dirname + './samples'));
+var fs = require('fs');
+var busboy = require('connect-busboy');
+app.use(busboy());
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
 
 app.post('/', (req,res) => {
-  var array = [];
-  var jsonObj = JSON.parse(req.body.json);
-  var keysArray = Object.keys(jsonObj);
-  var valuesArray = Object.values(jsonObj);
-  keysArray.pop();
-  var childArray = valuesArray.pop();
-  array = keysArray.concat(valuesArray);
-  function jsonParser(obj) {
+  console.log(__dirname);
+  var sampleFile = req.files.json;
+  uploadPath = __dirname + '/' + sampleFile.name;
+  sampleFile.mv(uploadPath, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('File Uploaded');
+  });
+  fs.readFile('sales_report.json', (err, data) => {
+    if(err) throw err;
+    const jsonObj = JSON.parse(data);
+    var keysArray = Object.keys(jsonObj);
+    var valuesArray = Object.values(jsonObj);
+    keysArray.pop();
+    var childArray = valuesArray.pop();
+    var csvText = keysArray.join(',') + '\n';
+    csvText += valuesArray.join(',') + '\n';
+    function jsonParser(obj) {
       for (var i = 0; i < obj.length; i++) {
           for(var keys in obj[i]) {
               if (keys === 'children' && obj[i][keys].length > 0) {
+                csvText += '\n';
                   jsonParser(obj[i].children);
               } else if (keys === 'children' && obj[i][keys].length === 0) {
+                csvText += '\n'
                   continue;
               } else {
-                  array.push(obj[i][keys]);
+                csvText += obj[i][keys] + ',';
               }
           }
       }
   }
-  jsonParser(childArray);
-  var txt = array.join(',');
-  //res.render('index');
-  //res.sendFile('index.html', {root: path.join(__dirname, './client')});
-  //res.write(txt);
-  res.send(`<div>${txt}</div><form action = "/" method="post"><label for ="json">Enter JSON</label><br><input class ="submissionfield" type="text" id="json" name="json"><br><button type="submit">Submit</button></form>`);
-  //res.send(txt);
+    jsonParser(childArray);
+    fs.writeFile(__dirname + '/sales_report.csv' , csvText, (err) => {
+      if (err) throw err;
+      console.log("File Downloaded Locally");
+    })
+    res.sendFile('index.html', {root: path.join(__dirname, './client')});
+    // res.send(`<div>${csvText}</div><form action = "/" method="post"><label for ="json">Enter JSON</label><br><input class ="submissionfield" type="file" id="json" name="json"><br><button type="submit">Submit</button></form>`);
+  })
 })
 
 app.get('/', (req, res) => {
@@ -44,3 +66,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App listening http://localhost:${port}`);
 })
+
