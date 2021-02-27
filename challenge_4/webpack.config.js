0bfad4cc81f616ccef345931a3a568a04c968ea5
vinayaachar <vinayaachar@gmail.com>
2021-02-26 22:34:00 -0800

    Complete Bare Minimum requirement

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './client/app.jsx',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, '/client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
